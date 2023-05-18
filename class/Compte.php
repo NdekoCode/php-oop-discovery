12344ccd795c2ca818e2761fe1758ca5716b09d7
NdekoCode <arickbulakali@gmail.com>
2023-05-18 08:03:05 +0200

    feat: add visibility in O0P

--- class/Compte.php
+++ class/Compte.php
@@ -10,24 +10,55 @@ class Compte
      *
      * @var string
      */
-    public string $titulaire;
+    private string $titulaire;
     /**
      * Solde du compte
      *
      * @var float
      */
-    public float $solde = 0;
+    private float $solde = 0;
     /**
      * La devise utiliser
      *
      * @var string
      */
-    public $currency = "$";
+    private $currency = "$";
     public function __construct(string $titulaire, float $montant = 20)
     {
-        $this->titulaire = $titulaire;
-        $this->solde += $montant;
+        $this->setTitulaire($titulaire);
+        $this->deposer($montant);
     }
+    public function setTitulaire(string $newTitulaire): void
+    {
+        if (is_string($newTitulaire) && strlen(trim($newTitulaire) >= 2)) {
+            $this->titulaire = $newTitulaire;
+        }
+    }
+    public function setSolde($newSolde): void
+    {
+        if ($newSolde > 0) {
+            $this->solde = $newSolde;
+        }
+    }
+    public function setCurrency($newCurrency): void
+    {
+        if (is_string($newCurrency) && strlen(trim($newCurrency)) > 0) {
+            $this->currency = $newCurrency;
+        }
+    }
+    public function getSolde(): float
+    {
+        return $this->solde;
+    }
+    public function getCurrency(): string
+    {
+        return $this->currency;
+    }
+    public function getTitulaire()
+    {
+        return $this->titulaire;
+    }
+
     public function deposer(float $montant)
     {
         if ($montant > 0) {
@@ -38,6 +69,7 @@ class Compte
     {
         echo "Vous avez $this->solde $this->currency dans votre compte";
     }
+
     public function retirer(float $montant)
     {
         if ($montant > 0 && ($this->solde - $montant) >= 10) {
